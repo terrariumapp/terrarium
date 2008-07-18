5844818b9cac63cba17390b79446a1f27ec84633
Bil Simser <emailme@bilsimser.com>
2008-07-18 13:34:08 +0000

    General code cleanup and reformatting, minor changes, code consistency cleanup.

--- Client/Controls/Classes/Controls/TerrariumLed.cs
+++ Client/Controls/Classes/Controls/TerrariumLed.cs
@@ -1,280 +1,230 @@
-//------------------------------------------------------------------------------
-//      Copyright (c) Microsoft Corporation.  All rights reserved.                                                              
-//------------------------------------------------------------------------------
-
-using System;
-using System.Collections;
 using System.ComponentModel;
 using System.Drawing;
-using System.Data;
 using System.Windows.Forms;
-
 using Terrarium.Glass;
 
 namespace Terrarium.Forms
 {
-	/// <summary>
-	/// 
-	/// </summary>
-	public class TerrariumLed : GlassButton
-	{
-		private System.Windows.Forms.ToolTip toolTip;
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TerrariumLed : GlassButton
+    {
+        private readonly GlassGradient _failedGradient = new GlassGradient(Color.FromArgb(255, 128, 128),
+                                                                           Color.FromArgb(128, 0, 0));
 
-		/// <summary>
-		///  Current state of the LED
-		/// </summary>
-		private LedStates state;
-		/// <summary>
-		///  Adds to the activity of the LED.  Active LEDs are in the 
-		///  waiting state
-		/// </summary>
-		private int activityCount = 0;
-		/// <summary>
-		///  Total number of completed LED activity requests.
-		/// </summary>
-		private int numRequests = 0;
-		/// <summary>
-		///  The name of this LED used in the Tool Tip description
-		/// </summary>
-		private string ledName = "";
+        private readonly GlassGradient _idleGradient = new GlassGradient(Color.FromArgb(128, 255, 128),
+                                                                         Color.FromArgb(0, 96, 0));
 
-		private GlassGradient idleGradient = new GlassGradient(Color.FromArgb(128, 255, 128), Color.FromArgb(0, 96, 0));
-		private GlassGradient waitingGradient = new GlassGradient(Color.FromArgb(255, 255, 128),Color.FromArgb(128, 128, 0));
-		private GlassGradient failedGradient = new GlassGradient(Color.FromArgb(255, 128, 128), Color.FromArgb(128, 0, 0));
+        private readonly GlassGradient _waitingGradient = new GlassGradient(Color.FromArgb(255, 255, 128),
+                                                                            Color.FromArgb(128, 128, 0));
 
-		/// <summary>
-		/// 
-		/// </summary>
-		public TerrariumLed()
-		{
-			InitializeComponent();
-			this.state = LedStates.Waiting;
-			this.NormalGradient = waitingGradient;
-		}
+        /// <summary>
+        ///  Adds to the activity of the LED.  Active LEDs are in the 
+        ///  waiting state
+        /// </summary>
+        private int _activityCount;
 
-		/// <summary>
-		///  Name for this LED.
-		/// </summary>
-		[Browsable(true)]
-		[Description("Set a LED name used in the tool tip")]
-		public string LedName
-		{
-			get
-			{
-				return ledName;
-			}
+        /// <summary>
+        ///  Current state of the LED
+        /// </summary>
+        private LedStates _currentLedState;
 
-			set
-			{
-				ledName = value;
-			}
-		}
+        /// <summary>
+        ///  The name of this LED used in the Tool Tip description
+        /// </summary>
+        private string _ledName = "";
 
-		/// <summary>
-		///  Current state of the LED, Idle, Waiting or Failed
-		///  See LedStates for more details or to add states.
-		/// </summary>
-		[Browsable(true)]
-		public LedStates LedState
-		{
-			get 
-			{
-				return state;
-			}
-        
-			set
-			{
-				if (this.Created == false)
-                //if ( this.Handle == IntPtr.Zero )
-				{
-					SetLedStateSynchronous(value);
-				}
-				else
-				{
-					this.BeginInvoke(new SetLedStateSynchronousCallback(SetLedStateSynchronous), new object[] {value});
-				}
-			}
-		}
+        /// <summary>
+        ///  Total number of completed LED activity requests.
+        /// </summary>
+        private int _numRequests;
 
-		/// <summary>
-		///  Asynchronous delegate for use with LedState property
-		/// </summary>
-		delegate void SetLedStateSynchronousCallback(LedStates newState);
+        private ToolTip _toolTip;
 
-		/// <summary>
-		///  Delegate implementation for asynchronous LedState changing
-		/// </summary>
-		/// <param name="newState">The new state of the LED</param>
-		public void SetLedStateSynchronous(LedStates newState)
-		{
-			if (newState == LedStates.Waiting)
-			{
-				numRequests++;
-			}
+        /// <summary>
+        /// 
+        /// </summary>
+        public TerrariumLed()
+        {
+            InitializeComponent();
+            _currentLedState = LedStates.Waiting;
+            NormalGradient = _waitingGradient;
+        }
 
-			state = newState;
+        /// <summary>
+        ///  Name for this LED.
+        /// </summary>
+        [Browsable(true)]
+        [Description("Set a LED name used in the tool tip")]
+        public string LedName
+        {
+            get { return _ledName; }
+            set { _ledName = value; }
+        }
 
-			this.UpdateGradient();
+        /// <summary>
+        ///  Current state of the LED, Idle, Waiting or Failed
+        ///  See LedStates for more details or to add states.
+        /// </summary>
+        [Browsable(true)]
+        public LedStates LedState
+        {
+            get { return _currentLedState; }
 
-			SetToolTip();
-			this.Invalidate();
-			this.Update();
-		}
+            set
+            {
+                if (Created == false)
+                {
+                    SetLedStateSynchronous(value);
+                }
+                else
+                {
+                    BeginInvoke(new SetLedStateSynchronousCallback(SetLedStateSynchronous), new object[] {value});
+                }
+            }
+        }
 
-		/// <summary>
-		///  Adds to the activity counter for the LED putting it into the
-		///  Waiting state.
-		/// </summary>
-		public void AddActivityCount()
-		{
-			this.BeginInvoke(new AddActivityCountSynchronousCallback(AddActivityCountSynchronous));
-		}
+        /// <summary>
+        ///  Provides access to the current activity count for an LED
+        /// </summary>
+        [Browsable(false)]
+        public int ActivityCount
+        {
+            get { return _activityCount; }
+        }
 
-		/// <summary>
-		///  Asynchronous delegate for adding to the activity counter
-		/// </summary>
-		delegate void AddActivityCountSynchronousCallback();
+        /// <summary>
+        ///  Delegate implementation for asynchronous LedState changing
+        /// </summary>
+        /// <param name="newState">The new state of the LED</param>
+        public void SetLedStateSynchronous(LedStates newState)
+        {
+            if (newState == LedStates.Waiting)
+            {
+                _numRequests++;
+            }
 
-		/// <summary>
-		///  Delegate implementation for async activity adds
-		/// </summary>
-		void AddActivityCountSynchronous() 
-		{
-			activityCount++;
-			LedState = LedStates.Waiting;
-			SetToolTip();
-		}
+            _currentLedState = newState;
 
-		/// <summary>
-		///  Used to subtract from the activity count asynchronously
-		/// </summary>
-		public void RemoveActivityCount() 
-		{
-			this.BeginInvoke(new RemoveActivityCountSynchronousCallback(RemoveActivityCountSynchronous));
-		}
+            updateGradient();
 
-		/// <summary>
-		///  Asynchronous delegate for subtracting from the activity counter
-		/// </summary>
-		delegate void RemoveActivityCountSynchronousCallback();
+            setToolTip();
+            Invalidate();
+            Update();
+        }
 
-		/// <summary>
-		///  Delegate implementation for async activity subtractions
-		/// </summary>
-		void RemoveActivityCountSynchronous() 
-		{
-			activityCount--;
-			if (activityCount <= 0)
-			{
-				activityCount = 0;
-				LedState = LedStates.Idle;
-			}
-			SetToolTip();
-		}
+        /// <summary>
+        ///  Adds to the activity counter for the LED putting it into the
+        ///  Waiting state.
+        /// </summary>
+        public void AddActivityCount()
+        {
+            BeginInvoke(new AddActivityCountSynchronousCallback(addActivityCountSynchronous));
+        }
 
-		/// <summary>
-		///  Provides access to the current activity count for an LED
-		/// </summary>
-		[Browsable(false)]
-		public int ActivityCount
-		{
-			get
-			{
-				return activityCount;
-			}
-		}
+        /// <summary>
+        ///  Delegate implementation for async activity adds
+        /// </summary>
+        private void addActivityCountSynchronous()
+        {
+            _activityCount++;
+            LedState = LedStates.Waiting;
+            setToolTip();
+        }
 
-		/// <summary>
-		///  Sets a detailed tool tip based on the led name, current state,
-		///  and activity.
-		/// </summary>
-		private void SetToolTip()
-		{
-			string extraMessage = "";
+        /// <summary>
+        ///  Used to subtract from the activity count asynchronously
+        /// </summary>
+        public void RemoveActivityCount()
+        {
+            BeginInvoke(new RemoveActivityCountSynchronousCallback(removeActivityCountSynchronous));
+        }
 
-			switch (LedState)
-			{
-				case LedStates.Idle:
-					extraMessage = "";
-					break;
-				case LedStates.Waiting:
-					extraMessage = "[Active:";
-					if (activityCount > 0)
-					{
-						extraMessage += " " + activityCount.ToString() + " requests";
-					}
-					extraMessage += "]";
-					break;
-				case LedStates.Failed:
-					extraMessage = "[Last Request Failed]";
-					break;
+        /// <summary>
+        ///  Delegate implementation for async activity subtractions
+        /// </summary>
+        private void removeActivityCountSynchronous()
+        {
+            _activityCount--;
+            if (_activityCount <= 0)
+            {
+                _activityCount = 0;
+                LedState = LedStates.Idle;
+            }
+            setToolTip();
+        }
 
-			}
+        /// <summary>
+        ///  Sets a detailed tool tip based on the led name, current state,
+        ///  and activity.
+        /// </summary>
+        private void setToolTip()
+        {
+            string extraMessage = "";
 
-			if ( this.toolTip != null )
-			{
-				this.toolTip.SetToolTip(this, ledName + " " + extraMessage + " [Total Requests: " + numRequests + "]");
-			}
-		}
+            switch (LedState)
+            {
+                case LedStates.Idle:
+                    extraMessage = "";
+                    break;
+                case LedStates.Waiting:
+                    extraMessage = "[Active:";
+                    if (_activityCount > 0)
+                    {
+                        extraMessage += " " + _activityCount + " requests";
+                    }
+                    extraMessage += "]";
+                    break;
+                case LedStates.Failed:
+                    extraMessage = "[Last Request Failed]";
+                    break;
+            }
 
-		#region Component Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.toolTip = new System.Windows.Forms.ToolTip();
-		}
-		#endregion
+            if (_toolTip != null)
+            {
+                _toolTip.SetToolTip(this, _ledName + " " + extraMessage + " [Total Requests: " + _numRequests + "]");
+            }
+        }
 
-		/// <summary>
-		///  Method for resetting the activity count and idle states for an LED
-		/// </summary>
-		private void Reset()
-		{
-			activityCount = 0;
-			LedState = LedStates.Idle;
-			SetToolTip();
-		}
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            _toolTip = new ToolTip();
+        }
 
-		///// <summary>
-		///// Override this to update our gradient, then let the base class
-		///// paint as normal.
-		///// </summary>
-		///// <param name="e"></param>
-		//protected override void OnPaint(PaintEventArgs e)
-		//{
-		//    switch( this.state )
-		//    {
-		//        case LedStates.Idle:
-		//            this.DisabledGradient = this.idleGradient;
-		//            break;
-		//        case LedStates.Waiting:
-		//            this.DisabledGradient = this.waitingGradient;
-		//            break;
-		//        case LedStates.Failed:
-		//            this.DisabledGradient = this.failedGradient;
-		//            break;
-		//    }
-		//    base.OnPaint (e);
-		//}
+        private void updateGradient()
+        {
+            switch (_currentLedState)
+            {
+                case LedStates.Idle:
+                    DisabledGradient = _idleGradient;
+                    break;
+                case LedStates.Waiting:
+                    DisabledGradient = _waitingGradient;
+                    break;
+                case LedStates.Failed:
+                    DisabledGradient = _failedGradient;
+                    break;
+            }
+        }
 
-		private void UpdateGradient()
-		{
-			switch( this.state )
-			{
-				case LedStates.Idle:
-					this.DisabledGradient = this.idleGradient;
-					break;
-				case LedStates.Waiting:
-					this.DisabledGradient = this.waitingGradient;
-					break;
-				case LedStates.Failed:
-					this.DisabledGradient = this.failedGradient;
-					break;
-			}
-		}
+        /// <summary>
+        ///  Asynchronous delegate for use with LedState property
+        /// </summary>
+        private delegate void SetLedStateSynchronousCallback(LedStates newState);
 
-	}
+        /// <summary>
+        ///  Asynchronous delegate for adding to the activity counter
+        /// </summary>
+        private delegate void AddActivityCountSynchronousCallback();
+
+        /// <summary>
+        ///  Asynchronous delegate for subtracting from the activity counter
+        /// </summary>
+        private delegate void RemoveActivityCountSynchronousCallback();
+    }
 }
