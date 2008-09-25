6854b3fed76b4b7883fbab3c378e700414e778b2
Bil Simser <emailme@bilsimser.com>
2008-09-25 04:17:52 +0000

    Stubbed out the various graphic engines to be added later.

---
+++ Client/Renderer/Classes/Engine/DirectX10GraphicsEngine.cs
@@ -0,0 +1,13 @@
+//------------------------------------------------------------------------------  
+//      Copyright (c) Microsoft Corporation.  All rights reserved.                                                             
+//------------------------------------------------------------------------------
+
+namespace Terrarium.Renderer.Engine
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DirectX10GraphicsEngine : IGraphicsEngine
+    {
+    }
+}
---
+++ Client/Renderer/Classes/Engine/IGraphicsEngine.cs
@@ -0,0 +1,13 @@
+//------------------------------------------------------------------------------  
+//      Copyright (c) Microsoft Corporation.  All rights reserved.                                                             
+//------------------------------------------------------------------------------
+
+namespace Terrarium.Renderer.Engine
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IGraphicsEngine
+    {
+    }
+}
