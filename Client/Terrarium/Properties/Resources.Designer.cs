c028bcbe10ef36613640fd0a90c049cdff112eb3
bsimser_cp <SND\bsimser_cp>
2008-11-26 20:32:22 +0000

    Had enough with incompatibilities and legacy code. Wiping away 2.0 and moving to the new 3.5 goodness.

--- Client/Terrarium/Properties/Resources.Designer.cs
+++ Client/Terrarium/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Terrarium.Glass.Properties {
+namespace Terrarium.Client.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Terrarium.Glass.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Terrarium.Glass.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Terrarium.Client.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -57,6 +57,13 @@ namespace Terrarium.Glass.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        internal static System.Drawing.Bitmap screensaver {
+            get {
+                object obj = ResourceManager.GetObject("screensaver", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
