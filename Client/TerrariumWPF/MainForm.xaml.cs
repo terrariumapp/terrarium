d467f0a9db2d5eb4a122fbb44217e79e71a70654
bsimser_cp <SND\bsimser_cp>
2008-11-27 17:21:57 +0000

    Added WPF versions of WinForms code. Also added WinForms 2 versions (not complete)

---
+++ Client/TerrariumWPF/MainForm.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Forms.Integration;
+using Terrarium.Renderer;
+
+namespace TerrariumWPF
+{
+    /// <summary>
+    /// Interaction logic for MainForm.xaml
+    /// </summary>
+    public partial class MainForm : Window
+    {
+        private readonly TerrariumDirectDrawGameView _tddGameView;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _tddGameView = new TerrariumDirectDrawGameView();
+            var host = new WindowsFormsHost {Child = _tddGameView};
+            tddGameView.Children.Add(host);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if(!DesignerProperties.GetIsInDesignMode(this))
+            {
+                if (_tddGameView.InitializeDirectDraw(false))
+                {
+                }
+            }
+        }
+    }
+}
