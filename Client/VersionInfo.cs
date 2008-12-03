4e771716a6cfd723621faa08b074bd864bde1344
Bil Simser <emailme@bilsimser.com>
2008-12-03 18:55:12 +0000

    Modified and added ClickOnce settings.

--- Client/VersionInfo.cs
+++ Client/VersionInfo.cs
@@ -5,12 +5,12 @@
 using System.Reflection;
 
 [assembly : AssemblyConfiguration("")]
-[assembly : AssemblyCompany("")]
+[assembly : AssemblyCompany("Microsoft")]
 [assembly : AssemblyProduct("Terrarium")]
 [assembly : AssemblyCopyright("")]
 [assembly : AssemblyTrademark("")]
 [assembly : AssemblyCulture("")]
-[assembly : AssemblyVersion("2.1.*")]
+[assembly : AssemblyVersion("2.1.0.*")]
 [assembly : AssemblyDelaySign(false)]
 
 [assembly : AssemblyKeyName("")]
